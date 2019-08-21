0da3e018198adfb9199314240001a82fc19c970e
Marius Mitrofan <marius.mitrofan@netbears.ro>
2019-08-21 13:51:28 +0300

    Downgrade git and seven_zip

--- metadata.rb
+++ metadata.rb
@@ -8,11 +8,12 @@ long_description 'Installs/Configures gitlab_stack'
 version          '1.0.0'
 
 depends 'build-essential', '~> 8.0.3'
-depends 'git'
+depends 'git', '~> 8.0.1'
 depends 'systemd', '~> 3.1.1'
 depends 'poise-service', '~> 1.5.2'
 depends 'ntp', '~> 3.5.1'
 depends 'logrotate', '~> 2.2.0'
+depends 'seven_zip', '~> 2.0.2'
 
 gem 'aws-sdk', '~> 2.7'
 
